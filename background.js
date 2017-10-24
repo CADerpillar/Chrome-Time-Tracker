60c252bced9a1a811f85142acc8a9d3b7dfc186f
Sp0ng3 <milanaugust@gmail.com>
2017-10-23 20:32:42 -0700

    Fixes Crashes
    So now I have removed the part of the program that actually saves the time from sessions because it breaks the program ATM.
    
    Should fix later.

--- background.js
+++ background.js
@@ -9,55 +9,12 @@ var minutes = 0;
 var hours = 0;
 var timeString = "";
 
-//Variables for getting saved values
-var LEGACYHOURS = 0;
-var LEGACYMINUTES = 0;
-var LEGACYSECONDS = 0;
-
-
 
 //GET THE VALUES IF THEY EXIST
-if(localStorage.getItem("UHOURS") != null){
-	 LEGACYHOURS = localStorage.getItem("UHOURS");
-}
-if(localStorage.getItem("UMINUTES") != null){
-	LEGACYMINUTES = localStorage.getItem("UMINUTES");
-}
-if(localStorage.getItem("USECONDS") != null){
-	LEGACYSECONDS = localStorage.getItem("USECONDS");
-}
 
 
 var timeTotal = ""; //For displaying total chrome usage
 
-
-//  Executes code on exit
-function save(){
-	
-	
-	
-	let saveHours = LEGACYHOURS + hours;
-	let saveMinutes = LEGACYMINUTES + minutes;
-	let saveSeconds = LEGACYSECONDS + seconds;
-	
-	if(saveSeconds > 60){
-		console.log("CALIBRATING SECONDS TO SAVE");
-		saveSeconds = 0;
-		saveMinutes += 1;	
-	}
-	console.log(saveSeconds.toString());
-	if(saveMinutes > 60){
-		console.log("CALIBRATING MINUTES TO SAVE");
-		saveMinutes = 0;
-		saveHours += 1;
-		
-	}
-	console.log(saveMinutes.toString());
-	timeTotal = saveHours.toString() + ":" + saveMinutes.toString() + ":" + saveSeconds.toString();
-	localStorage.setItem("UHOURS", hours);
-	localStorage.setItem("UMINUTES", minutes);
-	localStorage.setItem("USECONDS", seconds);
-}
 
 // Main function
 function main(){
@@ -80,15 +37,12 @@ function main(){
 		timeAbv = hours.toString() + ":" + minutes.toString();
 		chrome.browserAction.setBadgeText({text: timeAbv});
 		
-		save();
-		
 		//Change the DOCUMENT
 		var poppy = chrome.extension.getViews({
 			type: "popup"
 		});
 		for (var i = 0; i < poppy.length; i++){
 			poppy[i].document.getElementById('time').innerHTML = timeString;
-			poppy[i].document.getElementById('atime').innerHTML = timeTotal;
 		}
     /*
     TO-DO:  Set up the badge so it doesn't show hour or minutes when it is at zero.
